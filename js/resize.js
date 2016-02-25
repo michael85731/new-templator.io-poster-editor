182a1331554ea970e151d73f272cc0a1273e476c
Mic0L1n <michael85731@gmail.com>
2016-02-26 00:46:29 +0800

    font-size formula need adjustment, it have to creating an object to do that.

--- js/resize.js
+++ js/resize.js
@@ -1,21 +1,28 @@
-//儲存原先element的width,height
+//儲存原先element的originSize,width,height
 var originHeight = 0;
 var originWidth = 0;
+var originSize = 0
 
 //當text類型的div觸發resize事件時，能跟著變動text size
 function resizeText(target){
 	nowHeight = $(target).height();
 	nowWidth = $(target).width();
 	
-	//find min change persent and change style
-	var heightPersent = (nowHeight / originHeight) * 100;
-	var widthPersent = (nowWidth / originWidth) * 100;
-	var scale = Math.min(heightPersent,widthPersent) + "%";
-	$(target).css("font-size",scale);
+	//find minimize change and change style
+	var heightPersent = (nowHeight / originHeight);
+	var widthPersent = (nowWidth / originWidth);
+	var adjustment = Math.min(heightPersent,widthPersent);
+	console.log("height = " + heightPersent + "\n" + "width = " + widthPersent + "\n\n");
+	var unit = $(target).css("font-size").slice(-2);
+	if(adjustment != 0){
+		$(target).css("font-size",originSize * adjustment);
+	}
 }
 
 //讓文字變成resizable
 function resizableText(target){
+	unResizable(target);	//避免重複新增control point
+
 	var direction = ["nw","ne","sw","se","n","s","e","w"];
 	
 	//create control point
@@ -43,6 +50,25 @@ function resizableText(target){
 			'w': '#wgrip'
 		}
 	});
+
+	//紀錄當前數據
 	originWidth = $(target).width();
 	originHeight = $(target).height();
+	originSize = parseFloat($(target).css("font-size").slice(0,-2));
+}
+
+//取消resizable，不重複resizable element
+function unResizable(target){
+	$(".ui-resizable-handle").remove();
+	try{
+		$(target).resizable('destroy');
+	}catch(err){
+		//do nothing
+	}
+}
+
+//取消resize point
+function cancelResizePoint(){
+	$(".ui-resizable-handle").parent().resizable('destroy');
+	$(".ui-resizable-handle").remove();
 }
